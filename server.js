3eb1e7e0afd598c90cb1b9e9077bea573024406c
Winyumi <winguy2003@hotmail.com>
2019-12-14 23:57:59 -0500

    add all minimum required queries and made it modular

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql');
 const table = require('console.table');
+const query = require('./lib/queries');
 
-const db = function(query) {
-    let conn = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "test",
-        password: "test",
-        database: "employee_db"
-    });
-    conn.connect(function(err) {
-        if (err) throw err;
-        conn.query(query, function(err, result) {
-            console.table(result);
-        });
-        conn.end();
-    });
-};
-
-db(`SELECT * FROM employee`);
+query.viewEmployees();
 
 /*
 1. if database does not exist, source schema.sql
