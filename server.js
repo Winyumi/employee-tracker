5a15667d2ff9901bd03e53b2788e79ce49b24abe
Winyumi <winguy2003@hotmail.com>
2019-12-15 22:45:38 -0500

    add the rest of the minimum required menu options at their preliminary stages

--- server.js
+++ server.js
@@ -5,21 +5,82 @@ const query = require('./lib/query');
 const questions = require('./lib/questions');
 
 async function init() {
+    console.log('');
+    let departments = await query.viewDepartments();
+    let roles = await query.viewRoles();
+    let employees = await query.viewEmployees();
     let { main } = await inquirer.prompt(questions.main);
     let data;
     switch (main) {
+
         case 'View departments':
-            data = await query.viewDepartments();
-            console.table(data);
+            console.table(await query.viewDepartments());
             return init();
+
         case 'View roles':
-            data = await query.viewRoles();
-            console.table(data);
+            console.table(await query.viewRoles());
             return init();
+
         case 'View employees':
-            data = await query.viewEmployees();
-            console.table(data);
+            console.table(await query.viewEmployees());
             return init();
+
+        case 'Add department':
+            data = await inquirer.prompt(questions.addDepartment);
+            if (data.confirm) {
+                await query.addDepartment(data.name);
+                console.log(`Department "${data.name}" added.`);
+            }
+            return init();
+
+        case 'Add role':
+            data = await inquirer.prompt(questions.addRole);
+            if (data.confirm) {
+                if (data.department_id > departments.length) {
+                    console.error('Department id out of range. Role was not added.');
+                    return init();
+                }
+                await query.addRole(data.title, data.salary, data.department_id);
+                console.log(`Role "${data.title}" added.`);
+            }
+            return init();
+
+        case 'Add employee':
+            data = await inquirer.prompt(questions.addEmployee);
+            if (data.confirm) {
+                if (data.role_id > roles.length) {
+                    console.error('Role id out of range. Employee was not added.');
+                    return init();
+                }
+                if (data.manager_id > employees.length) {
+                    console.error('Manager id out of range. Employee was not added.');
+                    return init();
+                }
+                if (data.manager_id) {
+                    await query.addEmployee(data.first_name, data.last_name, data.role_id, data.manager_id);
+                } else {
+                    await query.addEmployee(data.first_name, data.last_name, data.role_id);
+                }
+                console.log(`Employee "${data.first_name} ${data.last_name}" added.`);
+            }
+            return init();
+
+        case 'Update role':
+            data = await inquirer.prompt(questions.updateEmployeeRole);
+            if (data.confirm) {
+                if (data.employee_id > employees.length) {
+                    console.error(`Employee ${data.employee_id} not found.`);
+                    return init();
+                }
+                if (data.role_id > roles.length) {
+                    console.error('Role id out of range. Employee was not changed.');
+                    return init();
+                }
+                await query.updateEmployeeRole(data.employee_id, data.role_id);
+                console.log(`Employee ${data.employee_id} updated.`);
+            }
+            return init();
+
     }
 };
 
