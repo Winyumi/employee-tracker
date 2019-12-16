1550776378ef90e95ab08171439d5e49d08fa32a
Winson Quon <winguy2003@gmail.com>
2019-12-16 17:46:04 -0500

    clean up comments

--- server.js
+++ server.js
@@ -225,31 +225,3 @@ async function init() {
 };
 
 init();
-
-/*
-query.viewDepartments();
-query.viewRoles();
-query.viewEmployees();
-*/
-/*
-1. if database does not exist, source schema.sql
-
-2. set up all functions
-    - add department
-    - add role
-    - add employee
-    - view departments
-    - view roles
-    - view employees
-    - update employees' roles
-
-    bonus:
-    - update managers
-    - view employees by manager
-    - delete department
-    - delete role
-    - delete employee
-    - view combined salaries of department
-
-3. set up inquirer and questions
-*/
